db049965b76de0120fabdc735b0c3b452a8e987f
AnaaSol <anasolm26@gmail.com>
2023-05-28 15:40:32 -0300

    inicio de sesion terminado + avances generales en html y server

--- TP3/Proyecto Integrador/modulos/classes.py
+++ TP3/Proyecto Integrador/modulos/classes.py
@@ -4,7 +4,6 @@ import datetime
 class Persona:
 
     def __init__(self):
-        self.DNI=""
         self.nombre=""
         self.apellido=""
         self.usuario=""
@@ -14,30 +13,33 @@ class Persona:
 
     def set_nombre(self, nombre):
         self.nombre=nombre
+        return nombre
 
     def set_apellido(self, apellido):
         self.apellido=apellido
-
-    def set_DNI(self, DNI):
-        self.DNI=DNI
+        return apellido
     
     def set_usuario(self, usuario):
         if isinstance (usuario, Persona):
             raise Exception ("Ya existe un usario con este nombre, por favor, ingresar otro")
         else:
             self.usuario=usuario
+            return usuario
 
     def set_contraseña(self, contraseña):
         self.contraseña=contraseña
+        return contraseña
 
     def set_mail(self, mail):
         if isinstance (mail, Persona):
             raise Exception ("Ya existe una cuenta con este mail, iniciar sección o ingresar uno distinto")
         else:
             self.mail=mail
+            return mail
     
     def set_claustro(self, claustro):
         self.usuario=claustro
+        return claustro
     
 
     def cambiar_datos(self, dato_a_cambiar, nuevo):
@@ -90,7 +92,7 @@ class Jefes(Persona):
 class Gestor_de_Reclamos:
     def clasificar(self, nombre_reclamo, descripcion):
         if "palabra clave" in nombre_reclamo:
-            depto=x
+            depto=""
         #...... repetir con varias palabras claves y hacer lo mismo con la descripcion
         return depto    
 
