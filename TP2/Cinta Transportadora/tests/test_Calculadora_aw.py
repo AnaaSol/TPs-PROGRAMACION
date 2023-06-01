8c7179d021df4eae767a746175844ac7e71edea6
PaulaB12 <paulabelendemartini2@gmail.com>
2023-05-31 22:45:07 -0300

    actualización en el diseño

--- TP2/Cinta Transportadora/tests/test_Calculadora_aw.py
+++ TP2/Cinta Transportadora/tests/test_Calculadora_aw.py
@@ -1,69 +1,49 @@
 import unittest
-from modulos.classes import Calculadora_aw
+from modulos.classes import *
 
 class TestCalculadora_aw(unittest.TestCase):
     
     def setUp(self):
-        self.calculadora = Calculadora_aw() 
+        self.calculadora = Calculadora_Aw() 
     
-    def test_calcular_aw(self):
-        """Prueba el cálculo correcto de la actividad acuosa de kiwis, manzanas, zanahorias y papas"""
-
-        #Se prueba el cálculo correcto del aw de un kiwi 
-        kiwi=[{"alimento": "kiwi", "peso": 0.55}]
-        awk=0.95999 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(kiwi, "kiwi"), awk, places=2)
-
-        #Se prueba el cálculo correcto del aw de una manzana
-        manzana=[{"alimento": "manzana", "peso": 0.35}]
-        awm=0.93604 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(manzana, "manzana"), awm, places=2)
-
-        #Se prueba el cálculo correcto del aw de una zanahoria
-        zanahoria=[{"alimento": "zanahoria", "peso": 0.3}]
-        awz=0.95999 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(zanahoria, "zanahoria"), awz, places=2)
-
-        #Se prueba el cálculo correcto del aw de una papa
-        papa=[{"alimento": "papa", "peso": 0.6}]
-        awp=0.97579 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(papa, "papa"), awp, places=2)
+    def test_calcular_aws(self):
+        """Prueba el cálculo correcto del promedio de la actividad acuosa de kiwis, manzanas, zanahorias, papas, frutas, verduras, y alimentos"""
 
         #Se prueba el cálculo correcto del aw promedio de varios kiwis
-        cajón_de_kiwis=[{"alimento": "kiwi", "peso": 0.12}, {"alimento": "kiwi", "peso": 0.3}, {"alimento": "kiwi", "peso": 0.09}]
+        kiwi=Kiwi(0.12)
+        kiwi2=Kiwi(0.3)
+        kiwi3=Kiwi(0.09)
+        cajón_de_kiwis=[kiwi, kiwi2, kiwi3]
         awk_prom=0.78521498 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(cajón_de_kiwis, "kiwi"), awk_prom, places=2)
+        self.assertAlmostEqual(self.calculadora.Calcular_aws(cajón_de_kiwis), awk_prom, places=2)
 
         #Se prueba el cálculo correcto del aw promedio de varias manzanas
-        cajón_de_manzanas=[{"alimento": "manzana", "peso": 0.3}, {"alimento": "manzana", "peso": 0.35}, {"alimento": "manzana", "peso": 0.3}]
+        mzna=Manzana(0.3)
+        mzna2=Manzana(0.35)
+        mzna3=Manzana(0.3)
+        cajón_de_manzanas=[mzna, mzna2, mzna3]
         awm_prom=0.92824842 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(cajón_de_manzanas, "manzana"), awm_prom, places=2)
+        self.assertAlmostEqual(self.calculadora.Calcular_aws(cajón_de_manzanas), awm_prom, places=2)
         
         #Se prueba el cálculo correcto del aw promedio de varias zanahorias
-        cajón_de_zanahorias=[{"alimento": "zanahoria", "peso": 0.3}, {"alimento": "zanahoria", "peso": 0.45}, {"alimento": "zanahoria", "peso": 0.2 }]
+        zana=Zanahoria(0.3)
+        zana2=Zanahoria(0.45)
+        zana3=Zanahoria(0.2)
+        cajón_de_zanahorias=[zana, zana2, zana3]
         awz_prom=0.89720597 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(cajón_de_zanahorias, "zanahoria"), awz_prom, places=2)
+        self.assertAlmostEqual(self.calculadora.Calcular_aws(cajón_de_zanahorias), awz_prom, places=2)
         
         #Se prueba el cálculo correcto del aw promedio de varias papas
-        cajón_de_papas=[{"alimento": "papa", "peso": 0.5}, {"alimento": "papa", "peso": 0.6}, {"alimento": "papa", "peso": 0.35}]
+        papa=Papa(0.5)
+        papa2=Papa(0.6)
+        papa3=Papa(0.35)
+        cajón_de_papas=[papa, papa2, papa3]
         awp_prom=0.95743682 #calculado
-        self.assertAlmostEqual(self.calculadora.Calcular_aw(cajón_de_papas, "papa"), awp_prom, places=2)
-
-        #Prueba que se lanza una excepción cuando el tipo de alimento ingresado y los alimentos del cajón no coinciden (o hay distintos tipos de alimentos en el cajón)
-        cajón_mixto=[{"alimento": "kiwi", "peso": 0.02}, {"alimento": "papa", "peso": 0.30}, {"alimento": "manzana", "peso": 0.35}]
-        self.calculadora.Calcular_aw(cajón_mixto, "kiwi")
-        self.calculadora.Calcular_aw(cajón_mixto, "manzana")
+        self.assertAlmostEqual(self.calculadora.Calcular_aws(cajón_de_papas), awp_prom, places=2)
 
         #Prueba que se lanza una excepción cuando no hay alimentos (el cajón está vacío)
         cajón_vacío=[]
-        self.calculadora.Calcular_aw(cajón_vacío, "kiwi")
-
-        #Prueba que se lanza una excepción cuando se quiere calcular la aw de un tipo de alimento con el que la calculadora no trabaja
-        cajón=[{"alimento": "kiwi", "peso": 0.02}, {"alimento": "papa", "peso": 0.30}]
-        self.calculadora.Calcular_aw(cajón, "zapallo")
-        self.calculadora.Calcular_aw(cajón, "tomate")
-        self.calculadora.Calcular_aw(cajón, "sandía")
-    
+        self.calculadora.Calcular_aws(cajón_vacío)
     
 if __name__ == '__main__':
     
--- TP2/Cinta Transportadora/tests/test_Cinta.py
+++ TP2/Cinta Transportadora/tests/test_Cinta.py
@@ -14,25 +14,6 @@ class TestCinta(unittest.TestCase):
         print(self.cinta.Llenar_cajón(10))
         print(self.cinta.Llenar_cajón(20))
     
-    def test_Separar(self):
-        """Prueba que se separe correctamente un cajón"""
-
-        #Prueba que los cajones de prueba se separen correctamente en kiwis, manzanas, zanahorias y papas
-        cajón1=self.cinta.Llenar_cajón(10)
-        cajón2=self.cinta.Llenar_cajón(25)
-        cajón3=self.cinta.Llenar_cajón(50)
-
-        print(self.cinta.Separar(cajón1))
-        print(self.cinta.Separar(cajón2))
-        print(self.cinta.Separar(cajón3))
-
-        #Prueba que se lanza una excepción cuando el cajón está vacío
-        cajón_vacío=self.cinta.Llenar_cajón(0)
-        self.cinta.Separar(cajón_vacío)
-
-    #def test_Analizar_calidad(self):
-    #    pass
-    
 if __name__ == '__main__':
     
     unittest.main()
