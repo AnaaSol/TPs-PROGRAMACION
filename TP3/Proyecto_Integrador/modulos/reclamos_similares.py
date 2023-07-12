7a463c2afce47e8579acb73ca89eba87041bc176
PaulaB12 <paulabelendemartini2@gmail.com>
2023-07-12 10:21:42 -0300

    cambios en reclamos_similares para rastrear el ID de los reclamos

--- TP3/Proyecto_Integrador/modulos/reclamos_similares.py
+++ TP3/Proyecto_Integrador/modulos/reclamos_similares.py
@@ -1,42 +1,51 @@
-from usuario import *
+#from usuario import *
 import nltk as nltk
 import collections
 
-def reclamos_similares (reclamos_same_depto, texto_objetivo): #reclamos_same_depto es una lista con la descripcion de los reclamos
-    #eliminar conectores/prefijos etc
+def reclamos_similares (reclamos_same_depto, texto_objetivo): #reclamos_same_depto es una lista de tuplas con la descripcion de los reclamos y su ID (descrip, ID)
+    
     cont=0
     similares=[]
-    lista_objetivo=[]
-    lista_reclamo=[]
-    lista_comparacion=[]
+    lista_objetivo=[] #lista de palabras del texto objetivo en orden de su frecuencia
+    lista_reclamo=[] #lista de palabras de los textos de mismo depto en orden de su frecuencia
+    lista_comparacion=[] #lista de lista_reclamos
+    
+    #filtro de palabras
     nltk.download('stopwords')
     stopwords_es=nltk.corpus.stopwords.words('spanish')
     palabras_filtradas_objetivo=[palabra for palabra in texto_objetivo.split() if palabra.lower() not  in stopwords_es]
-    texto_filtrado_objetivo = ' '.join(palabras_filtradas_objetivo)
-    counter1 = collections.Counter(texto_filtrado_objetivo.split())
-    for palabra, cont in counter1.most_common(): #el método most_comons retorna una lista de parejas ordenada en función del orden de apariciones, de mayor a menor
+    texto_filtrado_objetivo = ' '.join(palabras_filtradas_objetivo) #¿no es al cuete si después las separás de vuelta?
+    counter1 = collections.Counter(texto_filtrado_objetivo.split()) #diccionario que mapea cada elemento con su frecuencia
+    for palabra, cont in counter1.most_common(): #el método most_common retorna una lista de parejas ordenada en función del orden de apariciones, de mayor a menor
         lista_objetivo.append(palabra) #uso solo la palabra, descartando el numero de veces que se repite (pero manteniendo el orden)
-    #lista_objetivo=list(palabras_filtradas_objetivo)
+    
+    #mismo proceso para textos same depto
     for x in range(len(reclamos_same_depto)):
-        palabras_filtradas=[palabra for palabra in reclamos_same_depto[x].split() if palabra.lower() not  in stopwords_es]
+        palabras_filtradas=[palabra for palabra in reclamos_same_depto[x][0].split() if palabra.lower() not  in stopwords_es]
         texto_filtrado = ' '.join(palabras_filtradas)
         counter = collections.Counter(texto_filtrado.split())
         for palabra, cont in counter.most_common(): #el método most_comons retorna una lista de parejas ordenada en función del orden de apariciones, de mayor a menor
             lista_reclamo.append(palabra) #uso solo la palabra, descartando el numero de veces que se repite (pero manteniendo el orden)
-        lista_comparacion.append(lista_reclamo)
+        tupla=(lista_reclamo, reclamos_same_depto[x][1]) #tupla con la lista de palabras y el ID del reclamo
+        lista_comparacion.append(tupla)
         lista_reclamo=[]
+
+    #comparar las palabras significativas que aparecen y compararlas con el texto objetivo
     for i in lista_comparacion:
         for x in lista_objetivo:
-            if x in i:
+            if x in i[0]:
                 cont+=1
-            print (x, i)
+            #print (x, i)
         prom=(cont/len(i))*100
         if prom>=70:
-            indice=lista_comparacion.index(i)
-            similares.append(reclamos_same_depto[indice])
-        elif prom==100:
-            pass #agregar control
-        #comparar las palabras significativas que aparecen y compararlas con el texto objetivo
-    return similares #devuelve la descripcion del/los reclamo/s similar/es
+            similares.append(i[1])
+        
+    if len(similares)==0:
+        print("No se han encontrado similitudes")
+    else:
+        return similares #devuelve una lista con los ID del/los reclamo/s similar/es
 
-print(reclamos_similares(["esto es es una prueba una prueba prueba para evaluar el correcto funcionamiento de reclamos para evaluar reclamos similares"], "prueba evaluar el correcto funcionamiento")) #podriamos agregar un flitro para sinonimos
+#print(reclamos_similares(["esto es es una prueba una prueba prueba para evaluar el correcto funcionamiento de reclamos para evaluar reclamos similares"], "prueba evaluar el correcto funcionamiento")) #podriamos agregar un flitro para sinonimos
+lista_de_tuplas=[("tuqui", 37, "alapucha", 999), (1, 2), (1,3), (8, "boe ya basta")]
+print(lista_de_tuplas[0][0])
+print(lista_de_tuplas[3][1])
