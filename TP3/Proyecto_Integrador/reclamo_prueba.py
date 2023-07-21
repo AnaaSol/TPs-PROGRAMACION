a65942d05b176cdda0ae7fc168339683fb539307
AnaaSol <anasolm26@gmail.com>
2023-07-21 11:51:38 -0300

    agrego filtro para reclamos pendientes y arreglos sobre adherentes

--- TP3/Proyecto_Integrador/reclamo_prueba.py
+++ TP3/Proyecto_Integrador/reclamo_prueba.py
@@ -3,14 +3,17 @@ from modulos.databases import Reclamo_db
 from modulos.gestores import Gestor_de_base_de_datos
 import datetime
 
-# datos=["No hay papel higiénico en el módulo 1 durante toda la mañana", "pendiente", "maestranza", str(datetime.datetime.now())[:19], 6 ]
+# datos=["No hay papel higiénico en el módulo 1 durante toda la mañana", "pendiente", "maestranza", str(datetime.datetime.now())[:19], 4 ]
 # datos=["La señal de WiFi es muy baja en la sala de estudios", "pendiente", "soporte informático", str(datetime.datetime.now())[:19], 5]
 
 with app.app_context():
     db.create_all() #¿deberíamos crear la BD acá o en server?
-    # linea_a_eliminar = db.session.query(Reclamo_db).filter_by(ID_reclamo=2).first()
+    # linea_a_eliminar = db.session.query(Reclamo_db).filter_by(ID_reclamo=1).first()
     # db.session.delete(linea_a_eliminar)
     # db.session.commit()
     # GestorDB=Gestor_de_base_de_datos()
     # GestorDB.guardar_nuevo_objeto("reclamo", datos)
     # print("Todo espectacular")
+    # claim=db.session.query(Reclamo_db).filter_by(ID_user=4).first()
+    # print(claim.adherentes)
+    
